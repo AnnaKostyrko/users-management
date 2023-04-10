77fd577f6b002aab905189e7e4568d9d2bea6042
annakostyrko <aakostyrko@gmail.com>
2023-04-10 20:27:23 +0300

    Add a validation for get positions endpoint

--- src/users/positions.controller.ts
+++ src/users/positions.controller.ts
@@ -1,12 +1,22 @@
-import { Controller, Get, Param } from "@nestjs/common";
-import {PositionsService} from "./positions.service";
+import { Controller, Get } from "@nestjs/common";
+import { PositionsService } from "./positions.service";
 
 @Controller('positions')
 export class PositionsController {
     constructor(private readonly positionsService: PositionsService) {}
 
     @Get()
-    findAll() {
-        return this.positionsService.findAll();
+    async findAll() {
+        const position = await this.positionsService.findAll()
+        if (position.length === 0) {
+            return {
+                "success": false,
+                "message": "Positions not found"
+            }
+        }
+        return {
+            "success": true,
+            positions: await this.positionsService.findAll()
+        }
     }
 }
